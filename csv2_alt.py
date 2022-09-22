4a65f03b3c452bb90ac94b47c05de985476abda6
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-09-22 06:46:24 +0000

    update

--- csv2_alt.py
+++ csv2_alt.py
@@ -3,33 +3,39 @@ import csv
 import sys
 
 def write():
-    def em_in():
-        while True:
-            emno = int(input("Enter employee ID: "))
-            name = input("Enter employee name: ")
-            salr = float(input("Enter employee salary: "))
-            dept = input("Enter employee department: ")
-            w.writerow([emno, name, salr, dept])
-            c = input("Would you like to continue? (y/n) : ")
-            if c.lower() != 'y':
-                break
+    rec = []
+    while True:
+        emno = int(input("Enter employee ID: "))
+        name = input("Enter employee name: ")
+        salr = float(input("Enter employee salary: "))
+        dept = input("Enter employee department: ")
+        rec.append([emno, name, salr, dept])
+        c = input("Would you like to continue? (y/n) : ")
+        if c.lower() != 'y':
+            break
 
     file = open("employee.csv", "a")
     w = csv.writer(file)
     w.writerow(["Employee ID", "Employee Name", "Employee Salary", "Employee Department"])
-    em_in()
+    w.writerows(rec)
     file.close()
-    
+
 
 def read():
     file = open("employee.csv", "r")
     r = csv.reader(file)
     for i in r: print(i)
 
+
+def clear():
+    file = open("employee.csv", "w")
+    file.close()
+
 while True:
     print("""
 1. Write data to CSV file
 2. Read data from CSV file
+3. Clear CSV file
 0. Exit
     """)
 
@@ -40,6 +46,9 @@ while True:
 
     elif ch == 2:
         read()
+    
+    elif ch == 3:
+        clear()
 
     elif ch == 0:
         sys.exit("Thank You!")
