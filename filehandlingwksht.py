f8beb1a347818df3df47403433be97c21f7c93fa
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-10-23 13:03:45 +0000

    update

--- filehandlingwksht.py
+++ filehandlingwksht.py
@@ -1,3 +1,6 @@
+import pickle
+
+
 def replace():
     f = open("story.txt", "r")
     c = f.read()
@@ -102,6 +105,7 @@ def StartNotM():
             count += 1
     print(count)
 
+
 def startm():
     f = open("div.txt", "r")
     c = f.readlines()
@@ -112,16 +116,9 @@ def startm():
             count += 1
     print(count)
 
-def readl():
-    f=open("div.txt",'r')
-    str=''
-    while True:
-        str=f.readline()
-        print(str,end='')
-    f.close()
 
 def readlineonly():
-    f = open("div.txt", "r")
+    f = open("hello.txt", "r")
     m = ""
     while True:
         m += f.readline()
@@ -129,7 +126,6 @@ def readlineonly():
             break
     f.close()
     
-readlineonly()
 
 def copy():
     f = open("story.txt", "r")
@@ -139,6 +135,7 @@ def copy():
     f.write(c)
     f.close()
 
+
 def copyalternate():
     f = open("story.txt", "r")
     c = f.readlines()
@@ -147,3 +144,116 @@ def copyalternate():
     for i in range(0, len(c), 2):
         f.write(c[i])
     f.close()
+
+
+def copywordswithvowels():
+    f = open("World.txt", "r")
+    c = f.read()
+    f.close()
+    c = c.split()
+    f = open("Entire.txt", "w")
+    for i in c:
+        if i[0].lower() == "a" or i[0].lower() == "e" or i[0].lower() == "i" or i[0].lower() == "o" or i[0].lower() == "u":
+            f.write(i + " ")
+    f.close()
+
+
+def copyaandt():
+    f = open("World.txt", "r")
+    c = f.read()
+    f.close()
+    c = c.split()
+    f = open("Entire.txt", "w")
+    for i in c:
+        if i[0].lower() == "a" or i[0].lower() == "t":
+            f.write(i + " ")
+    f.close()
+
+
+def printlongestline():
+    f = open("World.txt", "r")
+    c = f.readlines()
+    f.close()
+    max = 0
+    for i in c:
+        if len(i) > max:
+            max = len(i)
+    print(max)
+
+
+def notrailingandleadingspaces():
+    f = open("story.txt", "r")
+    c = f.readlines()
+    f.close()
+    f = open("World.txt", "w")
+    for i in c:
+        print(i.strip() + "\n")
+    f.close()
+
+
+def displayallnumbers():
+    f = open("sumit.txt", "r")
+    c = f.read()
+    f.close()
+    for i in c:
+        print(i) if i.isdigit() == True else None
+    
+
+def displaysecondandsecondlast():
+    f = open("life.txt", "r")
+    c = f.readlines()
+    f.close()
+    print(c[1], c[-2])
+
+
+def addrec():
+    roomn = input("Enter room no: ")
+    price = input("Enter price: ")
+    rtype = input("Enter room type: ")
+    f = open("data.dat", "wb+")
+    pickle.dump([roomn, price, rtype], f)
+    f.close()
+
+
+def disp():
+    f = open("data.dat", "rb")
+    c = pickle.load(f)
+    f.close()
+    print(c)
+
+
+def specific_room(Room_No):
+    f = open("data.dat", "rb")
+    c = pickle.load(f)
+    f.close()
+    for i in c:
+        if c[0] == Room_No:
+            print("Price = ", c[1], "\nRoom Type = ", c[2])
+ 
+
+def modifi(Room_No):
+    f = open("data.dat", "rb+")
+    c = pickle.load(f)
+    f.close()
+    for i in c:
+        if c[0] == Room_No:
+            print("Price = ", c[1], "\nRoom Type = ", c[2])
+            c[1] = input("Enter new price: ")
+            c[2] = input("Enter new room type: ")
+            f = open("data.dat", "wb+")
+            pickle.dump(c, f)
+            f.close()
+            print("Updated")
+
+
+def delete(Room_No):
+    f = open("data.dat", "rb")
+    c = pickle.load(f)
+    f.close()
+    for i in c:
+        if c[0] == Room_No:
+            c.remove(i)
+    f = open("data.dat", "wb")
+    pickle.dump(c, f)
+    f.close()
+    print("Deleted")
