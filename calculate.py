c8fafe5d2916e7db5d3a87b99211bbbf47fdbf0d
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-10-26 06:55:39 +0000

    update

---
+++ calculate.py
@@ -0,0 +1,10 @@
+def add():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    print("Sum is ", a+b)
+
+
+def sub():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    print("Difference is ", a-b)
